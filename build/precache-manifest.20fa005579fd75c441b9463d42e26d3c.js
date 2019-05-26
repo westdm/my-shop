f5aae931720f2c244e82b287309af61a5b031e3c
Danae West <danae.west@gmail.com>
2019-05-25 23:08:42 -0600

    Initial commit

---
+++ build/precache-manifest.20fa005579fd75c441b9463d42e26d3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df37e6825b37f6a314c73d68e8e8b449",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65661427e4bd29f4f34b",
+    "url": "/static/css/main.c1fad18e.chunk.css"
+  },
+  {
+    "revision": "a95d539fc3a7d3d86478",
+    "url": "/static/js/2.939f9082.chunk.js"
+  },
+  {
+    "revision": "65661427e4bd29f4f34b",
+    "url": "/static/js/main.a009cb4b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ src/api/items.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    id: 1,
+    title: 'Knitting Pattern',
+    price: 5.00,
+    category: 'Patterns',
+  },
+  {
+    id: 2,
+    title: 'Knitting Pattern 2',
+    price: 7.50,
+    category: 'Patterns',
+  },
+  {
+    id: 3,
+    title: 'Knitting Pattern 3',
+    price: 9.00,
+    category: 'Patterns',
+  },
+];
